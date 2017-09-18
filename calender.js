c1183d9d656e630db11b0515f91ab2b26486d5f7
Jevon Wallace <jw0155039@otc.edu>
2017-09-18 12:34:14 -0500

    Looking Good, Need to Shift the days to apropriate start

--- calender.js
+++ calender.js
@@ -8,7 +8,7 @@ const moment = MomentRange.extendMoment(M)
 let year = moment().range('year')
 
 
-for(const month of year. by('month'))
+for (const month of year. by('month'))
 {
     console.log(_.pad(month.format('MMMM'), 26, '_'))
     console.log('S   M   T   W   Th   F   S ')
@@ -23,12 +23,26 @@ for(const month of year. by('month'))
         if (day.month() == 9 && day.date() == 10) 
        
         {
-            date = console.log(chalk.red(date))
-          
-            
+            date = chalk.red(date)
+   
         }
+
+       if (day.month() == 6 && day.date() == 21) 
+
+        {
+            date = chalk.magenta(date)
+   
+        }
+        
         return _.padEnd(date, 2 )
- 
+
     })
-    console.log(chalk.green(paddedDays))
+    
+    paddedDays = _.chunk(paddedDays, 7);
+    paddedDays.forEach(week => {console.log(week.join("  "))})
+
+
 }
+
+
+[]
